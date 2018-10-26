0e4b3c0d85c3e005a4f0d36c8c1278f4e90c1f1f
lipnus <sunpil13@korea.ac.kr>
2018-10-26 18:40:00 +0900

    update usertest database setting

--- routes/usertest/user.js
+++ routes/usertest/user.js
@@ -5,15 +5,8 @@ var path = require('path') // 상대경로
 var mysql = require('mysql') //express에 가면 mysql연동 정보가 있음
 
 // mysql연결
-var connection = mysql.createConnection({
-	host : 'ec2-13-125-247-189.ap-northeast-2.compute.amazonaws.com',
-	port : 3306,
-	user : 'root',
-	password : 'wptlel',
-	database : 'usertest'
-})
-
-connection.connect();
+var config = require('./function/config.js'); // AWS RDS연결
+var connection = config.db_connection;
 
 
 
